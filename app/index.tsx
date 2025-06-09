488ccfb3314fe8e0c28886b3717f55242cac389a
NoguchiHiroto <s2420648@u.tsukuba.ac.jp>
2025-06-09 18:32:39 +0900

    fix: パッケージのバージョンアップとたぶの追加

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,6 @@ import { View, Text } from 'react-native';
 import { Redirect } from 'expo-router';
 import React from 'react';
 
-const Route = () => <Redirect href="/input" />;
+const Route = () => <Redirect href="/graph" />;
 
 export default Route;
